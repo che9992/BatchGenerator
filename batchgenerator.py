b6097c989cdcd9e6532188c2bf8e6c1207b81f19
Chea <che9992@gmail.com>
2018-06-27 00:39:24 +0900

    Update batchgenerator.py

--- batchgenerator.py
+++ batchgenerator.py
@@ -1,13 +1,13 @@
 
 __author__ = "cheayoung jung <che9992@gmail.com>"
-__version__ = "2018-06-26"
+__version__ = "2018-06-27"
 
 
-    
 class BatchGenerator():
     where = 0
-        
-    '''
+    
+    
+        '''
     usage
     
     var = BatchGenerator(xdata, ydata, batch_size = 100)
@@ -34,17 +34,23 @@ class BatchGenerator():
 
     def next_batch(self):
         if self.where + self.batch_size > len(self.x_) :
-            self.where = len(self.x_) - self.where
-            print(self.where)
+            left = len(self.x_) - self.where
+            self.x = self.x_[len(self.x_) - left:]
+            self.y = self.y_[len(self.y_) - left:]
+            self.where = 0
             
-        self.x = self.x_[self.where:self.where+self.batch_size,]
-        self.y = self.y_[self.where:self.where+self.batch_size,]
-        self.where += self.batch_size
+            if self.one_hot:
+                self.set_one_hot()
+            
+            
+        else:
+            self.x = self.x_[self.where:self.where+self.batch_size,]
+            self.y = self.y_[self.where:self.where+self.batch_size,]
+            self.where += self.batch_size
         
-        if self.one_hot:
-            self.set_one_hot()
+            if self.one_hot:
+                self.set_one_hot()
         
     def set_one_hot(self):
-        self.y = np.int32(self.y)
         one_hot = np.array(self.y).reshape(-1)
         self.y = np.eye(self.nb_classes)[one_hot]
